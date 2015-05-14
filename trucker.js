9191bd298e18ec07fc408e61dc771ef813548793
Photon <phtn458@gmail.com>
2015-05-14 03:36:16 -0700

    saved added

--- trucker.js
+++ trucker.js
@@ -1,121 +1,181 @@
-
+Saved = new Mongo.Collection('saved');
 
 if (Meteor.isClient) {
 
-  Template.results.events({
-    'click .save-result': function(){
-      console.log('result saved.');
+// LANDING *** helpers
+
+// LANDING *** events
+  Template.landing.events({
+    'click .calc': function() {
+      //toast(document.querySelector('#calc-wc'));
+      //console.log('toast');
     },
-    'change #drive-toggle-button': function(){
-      $(".check-save").fadeIn('slow');
-      console.log('toggle changed.');
+    'click .saved-calc': function() {
+      Router.go('/saved');
     }
   });
 
+
+// RESULTS *** helpers
   Template.results.helpers({
     totalHours: function(){
       return Session.get('getTotalHours');
     },
     tripDuration: function(){
       return Session.get('getTripDuration');
+    },
+    drivingAs: function(){
+      return Session.get('getDrivingAs');
+    },
+    miles: function(){
+      return Session.get('getMiles');
+    },
+    speed: function(){
+      return Session.get('getSpeed');
     }
   });
 
+// RESULTS *** events
+  Template.results.events({
+    'click #save-results': function(){
+      Saved.insert({
+        saveMiles: Session.get('getMiles'),
+        saveSpeed: Session.get('getSpeed')
+
+      });
+      toast(document.querySelector('#save-toast'));
+    },
+    'click #driving-value': function(){
+      
+      if (Session.get('getDrivingAs') == 'Team'){
+        Session.set('getDrivingAs', 'Solo');
+        return Session.set('getTripDuration', getDurationCalc(Session.get('getRawHours'), 11));
+      } else {
+        Session.set('getDrivingAs', 'Team');
+        return Session.set('getTripDuration', getDurationCalc(Session.get('getRawHours'), 24));
+      }
+
+    }
+  });
+
+  
+// CALC *** helpers 
   Template.calc.helpers({
     totalHours: function(){
       return Session.get('getTotalHours');
     }
   });
 
+// CALC *** events
   Template.calc.events({
-    'click .check-save': function(){
+    'click .check-save': function() {
       console.log('check-save clicked');
     },
-    'keyup #speed-input': function(){
-      var s = $("#speed-input"), // speed-input
-          b = $(".get-btn"), // button
-          c = $(".get"); //check
+    'keyup #speed-input': function(e) {
+      calc(); // get total hours
 
-      if (s.val().length != 0 && $('#distance-input').val().length != 0){
-        b.prop('disabled', false);
-        b.css('background-color', '#34bf49');
+      if (e.which === 13) {
+        Router.go('/results');
+      } // enter key
 
-      } else {
-        b.prop('disabled', true);
-        c.css('color', '#fff');
-      }
-    },
-    'keyup #speed-input': function(){
+    }
 
+  });
+ // SAVED *** helpers
+ Template.saved.helpers({
+  saved: function() {
+    return Saved.find();
+  }
+ }); 
 
+ // SAVED *** events
+ Template.saved.events({
+  'click #exit': function() {
+    Router.go('/');
+  }
+ });
+
+ Session.set('getDrivingAs', 'Team');
+
+  var calc = function(){
       // Total Hours
-      var speedValue = $('#speed-input').val(),
-          distanceValue = $('#distance-input').val(),
-          rawHour = distanceValue / speedValue,
-          wholeHour = Math.floor(rawHour),
-          minDecimal = (rawHour % 1),
-          wholeMin = Math.floor(minDecimal * 60);
+      var   speedValue = $('#speed-input').val(),
+            distanceValue = $('#distance-input').val(),
+            rawHour = distanceValue / speedValue,
+            wholeHour = Math.floor(rawHour),
+            minDecimal = (rawHour % 1),
+            wholeMin = Math.floor(minDecimal * 60);
 
-      console.log(speedValue.length);
 
       if (speedValue.length == 2){
         $('.hours').fadeIn('slow');
 
-      Session.set('getTotalHours', wholeHour + ' hrs ' + wholeMin + ' mins' );
 
-      // Trip Duration
-      getDrive();
+        Session.set('getMiles', distanceValue);
+        Session.set('getSpeed', speedValue);
+        Session.set('getRawHours', rawHour);
 
-      var greater = function(hour){
+        // glimpse text
+        Session.set('getTotalHours', wholeHour + 'h ' + wholeMin + 'm' );
+        
 
-        var rawDays,
-            dayString = 'day';
-
-        if (wholeHour >= hour){ // >= Team Day
-          rawDays = wholeHour / hour;
-          var wholeDay = Math.floor(rawDays),
-              hourDecimal = (rawDays % 1),
-              dMinDecimal = (hourDecimal % 1),
-              fullHour = Math.floor(hourDecimal * hour),
-              dWholeMin = Math.floor(dMinDecimal * 60);
-
-
-          if (wholeDay > 1){
-            dayString += 's';
-          }
-
-          return Session.set('getTripDuration', wholeDay + ' ' + dayString + ' ' + fullHour + ' hrs  ' + dWholeMin + ' mins');
-        } else { // < Day
-          return Session.set('getTripDuration', wholeHour + ' hrs  ' + wholeMin + ' mins');
+       
+        if (Session.get('getDrivingAs') == 'Team'){
+          var driveHours = 24;
+        } else {
+          var driveHours = 11;
         }
-      };
+        
 
-      if (getDrive() == false){ // Team
-        greater(24);
-      } else { // Solo
-        greater(11);
-      }
-    } else if (speedValue.length == 1) {
-        $('.hours').fadeOut('slow');
-      };
+        Session.set('getTripDuration', (getDurationCalc(Session.get('getRawHours'), driveHours)));
+        //console.log(driveHours);
 
-  } // end of if speedValue.length == 2 
+      } else if (speedValue.length == 1) {
+          $('.hours').fadeOut('slow');
+        };
+
+    } // end of keyup #speed-input
+
+
+  var getDurationCalc = function (raw, driveHours){
+    
+          var rawDays = raw / driveHours;
+          //console.log(rawDays);
+          var hours = (rawDays % 1)*driveHours;
+          var mins = (hours % 1)*60;
+
+          if (rawDays < 1){
+            var teamTime = Math.floor(hours) + 'h ' + Math.floor(mins) + 'm';
+          } else {
+            var teamTime = Math.floor(rawDays) + 'd ' + Math.floor(hours) + 'h ' + Math.floor(mins) + 'm';
+          }
+          
+          return teamTime;
+        } // end of getDurationCalc
+
+
+
+
+  var toast = function(el){
+    el.show();
+  }
+
   
-
-  
-      
+} // end of isClient
 
 
 
-  });
-}
 
-// EXAMPLE FUNCTION
-var getDrive = function(){
-  var tog = $('#drive-toggle-button').prop('checked');
-  return tog;
-  console.log(tog);
+
+
+/* TOAST
+
+var save = function (el){
+  el.show();
 };
+
+*/
+
 
 
 
